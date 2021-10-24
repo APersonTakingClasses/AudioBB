41c2081bf225b8367886f37f28c9b32e35d88515
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2021-10-24 10:05:30 -0400

    created assignmment 7
    created book and booklist classes

---
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -0,0 +1,8 @@
+package edu.temple.audiobb
+
+import android.os.Bundle
+
+class Book(_title:String, _author:String) {
+    var title = _title
+    var author = _author
+}
---
+++ app/src/main/java/edu/temple/audiobb/BookList.kt
@@ -0,0 +1,9 @@
+package edu.temple.audiobb
+
+class BookList() {
+    // array of Books
+    var bookList = arrayListOf<Book>()
+    //init empty
+    //init given params, # of books, generated array
+
+}
