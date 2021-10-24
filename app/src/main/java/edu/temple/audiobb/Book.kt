2b0ad7c45f30a61d8a9a8d2276cb50fd58fcfede
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2021-10-24 11:24:12 -0400

    bookGenerater makes array of Books to feed to other objects

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -6,3 +6,5 @@ class Book(_title:String, _author:String) {
     var title = _title
     var author = _author
 }
+
+// val book1 = Book("DaRules","DaAuthor")
--- app/src/main/java/edu/temple/audiobb/BookList.kt
+++ app/src/main/java/edu/temple/audiobb/BookList.kt
@@ -6,4 +6,14 @@ class BookList() {
     //init empty
     //init given params, # of books, generated array
 
+    fun add(_book:Book){
+    }
+    fun remove(_book:Book){
+    }
+    fun get(_index:Int) : Book{
+        return Book("DaTitle","DaAuthor")
+    }
+    fun size():Int{
+        return bookList.size
+    }
 }
